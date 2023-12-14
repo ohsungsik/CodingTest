87870b37eccb6c8c7f56f76203e5883be5bb66ab
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:11:47 +0900

    [level unrated] Title: ad 제거하기, Time: 0.21 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    for (const string& s : strArr)
+    {
+        if (s.find("ad") == string::npos)
+        {
+            answer.push_back(s);
+        }
+    }
+    return answer;
+}
