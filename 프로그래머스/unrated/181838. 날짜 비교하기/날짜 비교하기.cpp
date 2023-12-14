f536d41bcccc6be1445ecd1a28ab628c3fbdf513
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:17:22 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer = 0;
+    
+    for (int i = 0; i < date1.size(); i++)
+    {
+        if (date1[i] == date2[i])
+            continue;
+        else if (date1[i] < date2[i])
+        {
+            answer = 1;
+            break;
+        }
+        else
+        {
+            answer = 0;
+            break;
+        }
+        
+    }
+        
+    return answer;
+}
