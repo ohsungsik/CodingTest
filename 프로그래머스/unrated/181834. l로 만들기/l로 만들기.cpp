af96cd14c02703499830fc50d41048a0f688229d
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:17:57 +0900

    [level unrated] Title: l로 만들기, Time: 0.31 ms, Memory: 3.78 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    for (char& c : myString)
+    {
+        if (c < 'l')
+            c = 'l';
+    }
+    
+    answer = myString;
+    
+    return answer;
+}
