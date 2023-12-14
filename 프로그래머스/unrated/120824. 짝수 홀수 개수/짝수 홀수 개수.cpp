da7c0b294c92845bf3e48c92a4c1007ba2a581d6
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:32:12 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer(2);
+    
+    for (const int& num : num_list)
+    {
+        if (num % 2 == 0)
+        {
+            answer[0]++;
+        }
+        else
+        {
+            answer[1]++;
+        }
+    }
+    
+    return answer;
+}
