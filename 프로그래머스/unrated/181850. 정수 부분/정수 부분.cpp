8da633b8831407c90d522067cec9df935b045e4d
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:15:47 +0900

    [level unrated] Title: 정수 부분, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = 0;
+    answer = static_cast<int>(floor(flo));
+    return answer;
+}
