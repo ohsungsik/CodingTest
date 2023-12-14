7fadffcb0d4d9e9b2dce8eab422e41e3d6700d6b
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:22:25 +0900

    [level unrated] Title: 세균 증식, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int t) {
+    int answer = n;
+    
+    while (t > 0)
+    {
+        answer *= 2;
+        t--;
+    }
+    
+    return answer;
+}
