9174dcae0456eb1a2c7c5d9d4b20f04edae5981a
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:04:08 +0900

    [level unrated] Title: 수 조작하기 1, Time: 0.18 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = 0;
+    for (int i = 0; i < control.size(); i++)
+    {
+        switch(control[i])
+        {
+            case 'w':
+                n += 1;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+        }
+    }
+    answer = n;
+    return answer;
+}
