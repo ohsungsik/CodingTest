22a5dc9ebaec3352fdb879b32d16e34df55a7fca
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:21:46 +0900

    [level unrated] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120922. 종이 자르기/종이 자르기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(int M, int N) {
+    int answer = 0;
+    
+    answer = M * N - 1;
+    
+    return answer;
+}
