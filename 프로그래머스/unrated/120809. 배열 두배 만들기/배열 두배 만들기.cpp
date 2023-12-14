438458881c9cc17bc6eff4132abc049fcc455e3a
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 23:16:33 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 0.15 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for (const int& number : numbers)
+    {
+        answer.push_back(number * 2);
+    }
+    return answer;
+}
