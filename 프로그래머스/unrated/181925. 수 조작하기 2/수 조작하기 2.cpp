d86678a801240a3ff047475fe085c7b50cf8936f
Sung-sik OH <148734055+sungsikoh@users.noreply.github.com>
2023-12-14 22:04:16 +0900

    [level unrated] Title: 수 조작하기 2, Time: 1.19 ms, Memory: 5.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for (int i = 0; i < numLog.size() - 1; i++)
+    {
+        int diff = numLog[i + 1] - numLog[i];
+        switch(diff)
+        {
+            case 1:
+                answer += "w";
+                break;
+            case -1:
+                answer += "s";
+                break;
+            case 10:
+                answer += "d";
+                break;
+            case -10:
+                answer += "a";
+                break;
+        }
+    }
+    return answer;
+}
